3b93627c241117cac96b1b084729938e85ab5b3c
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-07-13 17:07:19 -0500

    base cilantro

--- lib/clienteer.rb
+++ lib/clienteer.rb
@@ -1,5 +1,33 @@
 require "clienteer/version"
+require "clienteer/ingestor"
+require "clienteer/outgestor"
+
+require "mindbody-api"
 
 module Clienteer
-  # Your code goes here...
+
+  MindBody.configure do |config|
+    config.site_ids    = ENV["MINDBODY_SITE_ID"]
+    config.source_key  = ENV["MINDBODY_SOURCE_KEY"]
+    config.source_name = ENV["MINDBODY_SOURCE_NAME"]
+    config.log_level   = :debug # Savon logging level. Default is :debug, options are [:debug, :info, :warn, :error, :fatal]
+  end
+
+  def call
+    $skipped_people = []
+    job_definition = Kiba.parse do
+      source Ingestor::Mindbody
+      transform Digestor::IdealProteinCrossReference
+      transform Digestor::AddressCreation
+      transform Digestor::PhaseCreation
+      destination Outgestor::Maliero
+    end
+
+    Kiba.run job_definition
+
+    File.open("data/skipped_people" "a+") do |f|
+      f.write $skipped_people.join(",")
+    end
+  end
+
 end
