87b9e78eb07f47e510f5c285e6c1cf646623b4a2
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-07-14 14:30:35 -0500

    delete debugging output

--- lib/clienteer.rb
+++ lib/clienteer.rb
@@ -1,19 +1,22 @@
 require "clienteer/version"
 require "clienteer/ingestor"
 require "clienteer/outgestor"
+require "clienteer/digestor"
 
+require "kiba"
 require "mindbody-api"
 
 module Clienteer
 
-  MindBody.configure do |config|
-    config.site_ids    = ENV["MINDBODY_SITE_ID"]
+  ::MindBody.configure do |config|
+    config.site_ids    = ENV["MINDBODY_SITE_IDS"]
     config.source_key  = ENV["MINDBODY_SOURCE_KEY"]
     config.source_name = ENV["MINDBODY_SOURCE_NAME"]
     config.log_level   = :debug # Savon logging level. Default is :debug, options are [:debug, :info, :warn, :error, :fatal]
   end
 
-  def call
+  def self.call
+    $count = 0
     $skipped_people = []
     job_definition = Kiba.parse do
       source Ingestor::Mindbody
@@ -25,9 +28,10 @@ module Clienteer
 
     Kiba.run job_definition
 
-    File.open("data/skipped_people" "a+") do |f|
+    File.open("data/skipped_people", "a+") do |f|
       f.write $skipped_people.join(",")
     end
+
   end
 
 end
