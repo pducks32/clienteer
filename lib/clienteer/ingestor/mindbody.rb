f4f7de3a1146fbc11ecdfac0cd81be936631ce63
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-07-14 14:33:28 -0500

    fix saving raw issue

--- lib/clienteer/ingestor/mindbody.rb
+++ lib/clienteer/ingestor/mindbody.rb
@@ -3,17 +3,22 @@ module Clienteer
   module Ingestor
     class Mindbody
       def initialize
-        $progressbar = ProgressBar.create total: clients.length, format: '%a %bᗧ%i %p%% %t', progress_mark: ' ', remainder_mark: '･'
+        $progressbar = ProgressBar.create total: 2059, format: '%a %bᗧ%i %p%% %t', progress_mark: ' ', remainder_mark: '･'
+        @clients = get_clients
       end
 
-      def clients
-        @clients ||= MindBody::Services::ClientService.get_clients("UserCredentials"  => hash, "SearchText" => "").result
+      def get_clients
+        hash = {"Username" => ENV["MINDBODY_USERNAME"], "Password" => ENV["MINDBODY_PASSWORD"], "SiteIDs" => {"int" => ENV["MINDBODY_SITE_IDS"]}}
+        alpha = ::MindBody::Services::ClientService.get_clients("UserCredentials"  => hash, "SearchText" => "")
+        alpha.result[:clients]
       end
 
       def each
-        clients.each do |c|
-          $progressbar.increment
-          yield {raw: c} unless c.first_name.include?(" ") || c.last_name.include?(" ")
+        @clients.each do |c|
+          object = {raw: c}
+          unless c.first_name.nil? || c.last_name.nil? || c.first_name.include?(" ") || c.last_name.include?(" ") || c.first_name.include?("0") || c.email.nil? || c.nil?
+            yield object
+          end
         end
       end
     end
--- lib/clienteer/outgestor/maliero.rb
+++ lib/clienteer/outgestor/maliero.rb
@@ -1,4 +1,3 @@
-
 module Clienteer
   module Outgestor
     class Maliero
@@ -7,32 +6,43 @@ module Clienteer
       end
 
       def write(row)
-        @clients << Client.new do |c|
-          c.mindbody_id = row["raw"].id.to_s
-          c.birthdate = row["raw"].birth_date
-          c.gender = row["raw"].gender
+        $progressbar.increment
+        @clients << new_client(row)
+      end
+
+      def new_client(row)
+        Client.new do |c|
+          c.mindbody_id = row[:raw].id.to_s
+          c.birthdate = row[:raw].birth_date
+          c.gender = row[:raw].gender
           # c.blood_work = row["blood_work"]
           # c.constant_contact = row["constant_contact"]
-          c.email = row["raw"].email
-          c.first_name = row["raw"].first_name
+          c.email = row[:raw].email
+          c.first_name = row[:raw].first_name
           # c.health_profile = row["health_profile"]
           c.ideal_subscription_id = row["ideal_subscription_id"].to_s
           c.ideal_protein_subscription = !row["ideal_subscription_id"].nil?
-          c.last_name = row["raw"].last_name
+          c.last_name = row[:raw].last_name
           # c.needs_blood_work = row["needs_blood_work"]
           # c.newsletter = row["newsletter"]
-          c.notes = row["raw"].notes
+          c.notes = row[:raw].notes
           c.phase = row["phase"]
-          c.phone_number = row["raw"].phone_number
+          c.phone_number = row[:raw].home_phone || row[:raw].mobile_phone || nil
           c.address = row["address"]
         end
       end
 
       def close
+        $stderr.puts "\n\n\nHELLO\n\n\n\n"
+        binding.pry
         Client.transaction do
           success = @clients.map(&:save)
           unless success.all?
+            binding.pry
             errored = @clients.select{ |b| !b.errors.blank? }
+            errored.each do |e|
+              e.save!(validate: false)
+            end
             # do something with the errored values
             raise ActiveRecord::Rollback
           end
