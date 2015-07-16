350475bd866303be78c1e4fc7401a3cf46aaf682
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-07-16 12:35:04 -0500

    activesupport..support

--- lib/clienteer/digesters/address_creation.rb
+++ lib/clienteer/digesters/address_creation.rb
@@ -6,7 +6,7 @@ module Clienteer
       end
 
       def process(row)
-        row["address"] = Address.new(first_line: row[:raw].address_line1, second_line: row[:raw].address_line2, city: row[:raw].city.capitalize, state: row[:raw].state, zip_code: row[:raw].postal_code)
+        row["address"] = Address.new(first_line: row[:raw].address_line1, second_line: row[:raw].address_line2, city: row[:raw].city.try(:capitalize), state: row[:raw].state, zip_code: row[:raw].postal_code)
         row
       end
     end
