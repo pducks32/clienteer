fbc86a60b5c25e696571edc325543f9230dc0d41
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-07-16 11:31:29 -0500

    update railtie rake tasks

--- lib/clienteer/railtie.rb
+++ lib/clienteer/railtie.rb
@@ -2,9 +2,23 @@
 module Clienteer
   class Railtie < Rails::Railtie
     rake_tasks do
-      desc "Runs clienteer"
-      task :clienteer => :environment do
-        Clienteer.call
+      namespace :clienteer do
+        desc "Collect MindBody information to YAML file"
+        task collect: :environment do
+          Clienteer::Ingester::MindBody.to_file
+        end
+
+        namespace :run do
+          desc "Run Clienteer from mindbody.yml"
+          task file: :environment do
+            Clienteer.call ingestor: Clienteer::Ingester::YAMLFile
+          end
+
+          desc "Run Clienteer from new data"
+          task fresh: :collect do
+            Clienteer.call
+          end
+        end
       end
     end
   end
